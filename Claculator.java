c61c1a10c6666c0c357e2f898a87591f9f6d62c8
Shekar's Repository <chandrashekardeore29@gmail.com>
2022-06-21 09:52:16 +0530

    commit addition su btraction mul div

---
+++ Claculator.java
@@ -0,0 +1,76 @@
+package GIT;
+
+import java.util.Scanner;
+
+public class Claculator {
+	public static void main(String[] args)
+	{
+		Scanner s = new Scanner(System.in);
+		int n1,n2;
+		int ans=1, c=0;
+		int choice;
+		
+		do {
+			System.out.println("1.Addition\t 2.Subtraction\t 3.mul\t 4.Division\t 5.exit");
+			System.out.println("Enter your choice");
+			choice =s.nextInt();
+			
+			switch(choice)
+			{
+			case 1:
+				System.out.println("enter the numbers");
+				n1=s.nextInt();
+				n2=s.nextInt();
+				c=n1+n2;
+				System.out.println(n1+" + "+n2+"="+c);
+				break;
+			case 2:
+				System.out.println("enter the numbers");
+				n1=s.nextInt();
+				n2=s.nextInt();
+				if (n1>n2)
+				{
+					c=n1-n2;
+				}
+				else
+				{
+				c=n1-n2;
+				}
+				
+				System.out.println(n1+" - "+n2+"="+c);
+				break;
+			case 3:
+				System.out.println("enter the numbers");
+				n1=s.nextInt();
+				n2=s.nextInt();
+				c=n1*n2;
+				System.out.println(n1+" * "+n2+"="+c);
+				break;
+			case 4:
+				System.out.println("enter the numbers");
+				n1=s.nextInt();
+				n2=s.nextInt();
+				if (n2==0)
+				{System.out.println("dinoinator notnequal to 0");
+				}
+				else {
+					
+				c=n1/n2;
+				}
+				System.out.println(n1+" / "+n2+"="+c);
+				break;
+			case 5:
+				System.exit(0);
+				break;
+				
+			default:
+				System.out.println("Wrong choice!!");
+				
+			}
+			System.out.println("Do you want to contine(1/0)");
+			ans=s.nextInt();
+			
+		}while(ans!=0);
+	}
+
+}
